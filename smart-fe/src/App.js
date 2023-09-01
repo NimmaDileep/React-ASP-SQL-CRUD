b179046ad307fd3105d292642c596b3803c02058
dileepreddyoff@gmail.com <dileepreddyoff@gmail.com>
2023-09-01 13:14:44 -0400

    Fixed Eslint errors

--- smart-fe/src/App.js
+++ smart-fe/src/App.js
@@ -1,38 +1,38 @@
-import React, {useState} from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
-import Header from './components/Header/Header';
-import Footer from './components/Footer/Footer';
-import './App.css';
-import AuthContext from './components/AuthContext';
-import Main from "./components/Main/Main";
+import React, {useState} from 'react'
+import {BrowserRouter as Router} from 'react-router-dom'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import './App.css'
+import AuthContext from './components/AuthContext'
+import Main from "./components/Main/Main"
 
 function App() {
-    const initialToken = localStorage.getItem('accessToken');
-    const initialRole = localStorage.getItem('userRole');
-    const initialUserData = JSON.parse(localStorage.getItem('userData'));
-    const [authToken, setAuthToken] = React.useState(initialToken);
-    const [authRole, setAuthRole] = React.useState(initialRole);
-    const [userData, setUserData] = React.useState(initialUserData);
-    const [wasLoggedIn, setWasLoggedIn] = useState(false);
-    const [logoutClicked, setLogoutClicked] = useState(false);
+    const initialToken = localStorage.getItem('accessToken')
+    const initialRole = localStorage.getItem('userRole')
+    const initialUserData = JSON.parse(localStorage.getItem('userData'))
+    const [authToken, setAuthToken] = React.useState(initialToken)
+    const [authRole, setAuthRole] = React.useState(initialRole)
+    const [userData, setUserData] = React.useState(initialUserData)
+    const [wasLoggedIn, setWasLoggedIn] = useState(false)
+    const [logoutClicked, setLogoutClicked] = useState(false)
 
     React.useEffect(() => {
         if (authToken) {
             const tokenDuration = 1800000;
             const logoutTimer = setTimeout(() => {
-                setAuthToken(null);
-                setAuthRole(null);
-                setUserData(null);
-                localStorage.removeItem('accessToken');
-                localStorage.removeItem('userRole');
-                localStorage.removeItem('userData');
-            }, tokenDuration);
+                setAuthToken(null)
+                setAuthRole(null)
+                setUserData(null)
+                localStorage.removeItem('accessToken')
+                localStorage.removeItem('userRole')
+                localStorage.removeItem('userData')
+            }, tokenDuration)
 
             return () => {
-                clearTimeout(logoutTimer);
+                clearTimeout(logoutTimer)
             }
         }
-    }, [authToken]);
+    }, [authToken])
 
     return (
         <Router>
@@ -45,4 +45,4 @@ function App() {
     );
 }
 
-export default App;
+export default App
